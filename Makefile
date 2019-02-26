df4cc7d1d67a28556371f0f8feda2b567616e795
nnashiki <n.nashiki.work@gmail.com>
2019-02-26 13:54:40 +0900

    update: 画面の更新ができた

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
-	docker run --name sample1 -p 8090:80 -v `pwd`/src:/usr/share/nginx/html -d nginx
+	docker run --rm --name sample1 -p 8090:80 -v `pwd`/src:/usr/share/nginx/html -d nginx
+
+stop:
+	docker stop sample1
--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ var app = new Vue({
     }
 })
 
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        counter: 0
+    },
+    methods: {
+        increment: function (event) {
+            this.counter += 1
+        }
+    }
+})
+
 var button = new Vue({
     el: '#button',
     methods: {
-       handleClick : function (event) {
-           alert(event.target)
-       }
+        handleClick: function (event) {
+            alert(event.target)
+        }
     }
 })
